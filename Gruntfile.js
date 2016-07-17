891c6073260bce5a453f8ad22e94a6d578402913
Johannes Neumeier <hello@johannesneumeier.com>
2016-07-17 20:58:47 +0300

    first commit, jquery boilerplate with some initial code

---
+++ Gruntfile.js
@@ -0,0 +1,75 @@
+module.exports = function( grunt ) {
+
+	grunt.initConfig( {
+
+		// Import package manifest
+		pkg: grunt.file.readJSON( "package.json" ),
+
+		// Banner definitions
+		meta: {
+			banner: "/*\n" +
+				" *  <%= pkg.title || pkg.name %> - v<%= pkg.version %>\n" +
+				" *  <%= pkg.description %>\n" +
+				" *  <%= pkg.homepage %>\n" +
+				" *\n" +
+				" *  Made by <%= pkg.author.name %>\n" +
+				" *  Under <%= pkg.license %> License\n" +
+				" */\n"
+		},
+
+		// Concat definitions
+		concat: {
+			options: {
+				banner: "<%= meta.banner %>"
+			},
+			dist: {
+				src: [ "src/jquery.fontsampler.js" ],
+				dest: "dist/jquery.fontsampler.js"
+			}
+		},
+
+		// Lint definitions
+		jshint: {
+			files: [ "src/jquery.fontsampler.js", "test/**/*" ],
+			options: {
+				jshintrc: ".jshintrc"
+			}
+		},
+
+		jscs: {
+			src: "src/**/*.js",
+			options: {
+				config: ".jscsrc"
+			}
+		},
+
+		// Minify definitions
+		uglify: {
+			dist: {
+				src: [ "dist/jquery.fontsampler.js" ],
+				dest: "dist/jquery.fontsampler.min.js"
+			},
+			options: {
+				banner: "<%= meta.banner %>"
+			}
+		},
+		// watch for changes to source
+		// Better than calling grunt a million times
+		// (call 'grunt watch')
+		watch: {
+			files: [ "src/*", "test/**/*" ],
+			tasks: [ "default" ]
+		}
+
+	} );
+
+	grunt.loadNpmTasks( "grunt-contrib-concat" );
+	grunt.loadNpmTasks( "grunt-contrib-jshint" );
+	grunt.loadNpmTasks( "grunt-jscs" );
+	grunt.loadNpmTasks( "grunt-contrib-uglify" );
+	grunt.loadNpmTasks( "grunt-contrib-watch" );
+
+	grunt.registerTask( "lint", [ "jshint", "jscs" ] );
+	grunt.registerTask( "build", [ "concat", "uglify" ] );
+	grunt.registerTask( "default", [ "jshint", "build" ] );
+};
---
+++ wishlist.md
@@ -0,0 +1,13 @@
+#Wishlist
+
+* single line vs paragraph previews
+* in input editor vs linked separate input field
+* font size switcher
+* line-height switcher
+* letter-spacing switcher
+* customizable style and layout
+* dynamic font loading (switching)
+* shorthands for changing the text sample (default options)
+
+* webfont obfuscation / character subsets (?)
+* wordpress plugin with font file upload and manager
